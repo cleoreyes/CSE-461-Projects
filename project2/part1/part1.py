90a01913c76337df2774a40e272b0b142202f36d
“Hazel” <hazelzh7@cs.washington.edu>
2025-05-04 02:16:36 -0700

    Update part1.py

--- project2/part1/part1.py
+++ project2/part1/part1.py
@@ -1,1 +1,35 @@
 
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.cli import CLI
+
+
+class part1_topo(Topo):
+    def build(self):
+        # 1. Create a switch
+        s1 = self.addSwitch( 's1', failMode='standalone' )
+
+        # 2. 4 hosts
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        h4 = self.addHost( 'h4' )
+
+        # 3. Link each host to s1
+        self.addLink( h1, s1 )
+        self.addLink( h2, s1 )
+        self.addLink( h3, s1 )
+        self.addLink( h4, s1 )
+
+
+topos = {"part1": part1_topo}
+
+if __name__ == "__main__":
+    t = part1_topo()
+    net = Mininet(topo=t, controller=None)
+    net.start()
+    CLI(net)
+    net.stop()
